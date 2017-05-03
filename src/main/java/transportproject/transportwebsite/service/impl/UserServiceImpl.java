1b7af0661f17f39f46278e9bf428b634bdb79958
MINSK\Maksim_Radzevich <Maksim_Radzevich@epam.com>
2017-05-03 13:14:09 +0300

    maven dependency plugin

--- src/main/java/transportproject/transportwebsite/service/impl/UserServiceImpl.java
+++ src/main/java/transportproject/transportwebsite/service/impl/UserServiceImpl.java
@@ -15,40 +15,7 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     public UserServiceImpl(UserDAO userDAO) {
-        this.userDAO = userDAO;ngframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping register
-        INFO: Mapped "{[/test],methods=[GET]}" onto public java.lang.String transportproject.transportwebsite.test.TestController.testTransport()
-        апр 29, 2017 11:21:32 PM org.springframework.web.servlet.handler.SimpleUrlHandlerMapping registerHandler
-        INFO: Mapped URL path [/static/**] onto handler of type [class org.springframework.web.servlet.resource.ResourceHttpRequestHandler]
-         апр 29, 2017 11:21:33 PM org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter initControllerAdviceCache
-         INFO: Looking for @ControllerAdvice: Root WebApplicationContext: startup date [Sat Apr 29 23:21:29 MSK 2017]; root of context hierarchy
-         SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder".
-         SLF4J: Defaulting to no-operation (NOP) logger implementation
-         SLF4J: See http://www.slf4j.org/codes.html#StaticLoggerBinder for further details.
-         апр 29, 2017 11:21:33 PM org.springframework.web.context.ContextLoader initWebApplicationContext
-         INFO: Root WebApplicationContext: initialization completed in 4341 ms
-         апр 29, 2017 11:21:33 PM org.springframework.web.servlet.DispatcherServlet initServletBean
-         INFO: FrameworkServlet 'dispatcher': initialization started
-         [INFO] Initializing Spring FrameworkServlet 'dispatcher'
-         апр 29, 2017 11:21:33 PM org.springframework.web.context.support.AnnotationConfigWebApplicationContext prepareRefresh
-         INFO: Refreshing WebApplicationContext for namespace 'dispatcher-servlet': startup date [Sat Apr 29 23:21:33 MSK 2017]; parent: Root WebApplicationContext
-         апр 29, 2017 11:21:33 PM org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor <init>
-         INFO: JSR-330 'javax.inject.Inject' annotation found and supported for autowiring
-         апр 29, 2017 11:21:33 PM org.springframework.web.servlet.DispatcherServlet initServletBean
-         [INFO] Started o.e.j.m.p.JettyWebAppContext@2401856{/,file:///Users/maximradevich/IdeaProjects/TransportProject/src/main/webapp/,AVAILABLE}{file:///Users/maximradevich/IdeaProjects/TransportProject/src/main/webapp/}
-         INFO: FrameworkServlet 'dispatcher': initialization completed in 53 ms
-         [INFO] Started ServerConnector@47666b39{HTTP/1.1,[http/1.1]}{0.0.0.0:8080}
-         [INFO] Started @12065ms
-         [INFO] Started Jetty Server
-         Hibernate:
-         select
-         this_.transport_id as transpor1_3_0_,
-         this_.route_number as route_nu2_3_0_,
-         this_.type as type3_3_0_
-         from
-         Transport this_
-         where
-         this_.type=?
-         Hibernate:
+        this.userDAO = userDAO;
     }
 
     private static String getActiveUserEmail() {

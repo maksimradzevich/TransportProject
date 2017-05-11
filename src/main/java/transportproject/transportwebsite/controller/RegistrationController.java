b513c070afab9efd722a8534d8d8d3561ae439b9
ngen277 <ngen277@gmail.com>
2017-05-11 23:53:33 +0300

    another improvements

--- src/main/java/transportproject/transportwebsite/controller/RegistrationController.java
+++ src/main/java/transportproject/transportwebsite/controller/RegistrationController.java
@@ -4,21 +4,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import transportproject.transportwebsite.dao.UserDAO;
+import transportproject.transportwebsite.helper.RegistrationHelper;
+import transportproject.transportwebsite.helper.exceptions.UserExistsException;
 import transportproject.transportwebsite.model.User;
 
 @Controller
 public class RegistrationController {
     private final UserDAO userDAO;
+    private final RegistrationHelper registrationHelper;
 
     @Autowired
-    public RegistrationController(UserDAO userDAO) {
+    public RegistrationController(UserDAO userDAO, RegistrationHelper registrationHelper) {
         this.userDAO = userDAO;
+        this.registrationHelper = registrationHelper;
     }
 
     @GetMapping("/registration")
     public String registrationPage(Model model) {
         model.addAttribute("user", new User());
         return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String register(User user, Model model) {
+        try {
+            registrationHelper.registerUser(user);
+        } catch (UserExistsException e) {
+            model.addAttribute("exception", e);
+            return "registration";
+        }
+        return "redirect:login";
     }
 }
--- src/main/webapp/static/js/reg.js
+++ src/main/webapp/static/js/reg.js
@@ -5,8 +5,16 @@ function regController() {
 
     vm.user = {
         email: "",
-        password: ""
+        password: "",
+        passwordRetype: ""
     };
+
+    vm.formValid = vm.regform.$invalid && vm.user.password === vm.user.passwordRetype;
 }
 
 module.controller("regCont", [regController]);
+
+function loginController() {
+
+}
+module.controller("login", [loginController]);

082c63939f1aa25c5bcc5792f8a8195b46f58eae
ngen277 <ngen277@gmail.com>
2017-05-14 19:25:01 +0300

    datasource.properties deletion

--- src/main/java/transportproject/transportwebsite/configuration/HibernateConfiguration.java
+++ src/main/java/transportproject/transportwebsite/configuration/HibernateConfiguration.java
@@ -28,7 +28,6 @@ import java.util.Properties;
 @ComponentScan({"transportproject.transportwebsite.configuration"})
 public class HibernateConfiguration {
 
-    private static final String DATASOURCE_PROPERTIES_PATH = "datasource.properties";
     private static final String HIBERNATE_PROPERTIES_PATH = "hibernate.properties";
 
     @Bean
@@ -58,11 +57,10 @@ public class HibernateConfiguration {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setInitialSize(1);
         dataSource.setMaxIdle(5);
-        final Properties properties = getProperties(DATASOURCE_PROPERTIES_PATH);
         dataSource.setUrl(System.getenv("JDBC_DATABASE_URL"));
-        dataSource.setDriverClassName((String) properties.getProperty("jdbc.driverClassName"));
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUsername(System.getenv("JDBC_DATABASE_USERNAME"));
-        dataSource.setPassword((String) properties.getProperty("JDBC_DATABASE_PASSWORD"));
+        dataSource.setPassword(System.getenv("JDBC_DATABASE_PASSWORD"));
 
         return dataSource;
     }

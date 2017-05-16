dbdc8dd5d61a27eb819821102dcbd4acc9ceed52
ngen277 <ngen277@gmail.com>
2017-05-16 22:16:40 +0300

    improvements in exceptioncontroller

--- src/main/java/transportproject/transportwebsite/controller/ExceptionController.java
+++ src/main/java/transportproject/transportwebsite/controller/ExceptionController.java
@@ -3,11 +3,14 @@ package transportproject.transportwebsite.controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import transportproject.transportwebsite.service.exceptions.NotFoundException;
+import transportproject.transportwebsite.service.exceptions.UserExistsException;
 
 @ControllerAdvice
 public class ExceptionController {
 
-    @ExceptionHandler(value = Exception.class)
+    @ExceptionHandler(value = {NotFoundException.class, UserExistsException.class, NoHandlerFoundException.class})
     public String errorPage(Model model, Exception e) {
         model.addAttribute("message", e.getMessage());
         return "error";

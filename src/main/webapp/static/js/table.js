0d69aa6f952a7ab8b9e8b45aee67f5650c2cd00c
ngen277 <ngen277@gmail.com>
2017-05-17 08:58:24 +0300

    improvements in exceptioncontroller

--- src/main/webapp/static/js/table.js
+++ src/main/webapp/static/js/table.js
@@ -1,40 +1,24 @@
-
-
 function addTable(index) {
 
     console.log("CALL addTable with index: " + index);
 
     var timetable = document.getElementById("t" + index);
     var stringJson = timetable.innerHTML;
-    var json = JSON.parse(stringJson);
-    var tableOfTimetable = createTableOfTimetable(json);
-    tableOfTimetable.className = "table table-bordered";
-    document.getElementById("timetable" + index).appendChild(tableOfTimetable);
+    var tableOfTimetable;
+    try {
+        var json = JSON.parse(stringJson);
+        tableOfTimetable = createTableOfTimetable(json);
+        tableOfTimetable.className = "table table-bordered";
+        document.getElementById("timetable" + index).appendChild(tableOfTimetable);
+    } catch (e){
+        tableOfTimetable = new DOMParser().parseFromString(stringJson, "text/xml");
+        // tableOfTimetable.documentElement.className = "table table-bordered";
+        document.getElementById("timetable" + index).appendChild(tableOfTimetable.documentElement);
+        // tableOfTimetable = $.parseHTML(stringJson);
+        // tableOfTimetable.className = "table table-bordered";
+        // $("#timetable" + index).append(tableOfTimetable);
+    }
 }
-
-// var timetable = document.getElementById("t");
-// var stringJson = timetable.innerHTML;
-// var json = JSON.parse(stringJson);
-// var tableOfTimetable = createTableOfTimetable(json);
-// tableOfTimetable.className = "table table-bordered";
-// document.getElementById("timetable").appendChild(tableOfTimetable);
-
-//
-// for (var i = 0; i < numberOfInner; i++) {
-//     var tableRow = document.createElement("TR");
-//
-//     for (var prop in json[i]) {
-//
-//         var newVar = json[i][prop];
-//         console.log(newVar);
-//         var text = document.createTextNode(newVar);
-//         var tableCell = document.createElement("TD");
-//         tableCell.appendChild(text);
-//         tableRow.appendChild(tableCell);
-//         // array.push(objectArray[i][prop])
-//     }
-//     document.getElementById("timetable").appendChild(tableRow);
-// }
 
 function createTableOfTimetable(json) {
     var table = document.createElement("TABLE");
